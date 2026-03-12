334dcd6786940e670b86fe30579dcf647b37fa0d
Dmitry Nazarov <roygbiv1969@yandex.ru>
2026-03-12 23:52:36 +0300

    Camera Controller/Doors/RoomActivator

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static CameraController instance;
+
+    public float moveSpeed;
+
+    public Transform target;
+
+    private void Awake()
+    {
+        instance = this; 
+    }
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), moveSpeed * Time.deltaTime);
+
+        }
+
+    }
+
+    public void ChangeTarget(Transform newTarget)
+    {
+        target = newTarget; 
+    }
+}
