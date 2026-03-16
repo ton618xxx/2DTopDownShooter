48c2f364246097173f961deed38255d994c42dd0
Dmitry_Nazarov <roygbiv1969@yandex.ru>
2026-03-16 20:54:50 +0300

    Level Flow Updated/Exit/Fade

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static LevelManager instance;
+
+    public float waitToLoad = 4f;
+
+    public string nextLevel; 
+
+    private void Awake()
+    {
+        instance = this;    
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator LevelEnd()
+    {
+        AudioManager.instance.PlayLevelWin();
+
+        PlayerController.instance.canMove = false;
+
+        UIController.instance.StartFadeToBlack();   
+
+        yield return new WaitForSeconds(waitToLoad);
+
+        SceneManager.LoadScene(nextLevel);
+
+    }
+}
--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -11,7 +11,12 @@ public class UIController : MonoBehaviour
     public Slider healthSlider;
     public Text healthText;
 
-    public GameObject deathScreen; 
+    public GameObject deathScreen;
+
+    public Image fadeScreen;
+    public float fadeSpeed;
+    private bool fadeToBlack, fadeOutBlack; 
+
 
     private void Awake()
     {
@@ -21,11 +26,34 @@ public class UIController : MonoBehaviour
 
     void Start()
     {
-        
+        fadeOutBlack = true;
+        fadeToBlack = false;    
     }
 
     void Update()
     {
-        
+        if (fadeOutBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 0f, fadeSpeed * Time.deltaTime));
+            if(fadeScreen.color.a == 0f)
+            {
+                fadeOutBlack = false;    
+            }
+        }
+
+        if(fadeToBlack)
+        {
+            fadeScreen.color = new Color(fadeScreen.color.r, fadeScreen.color.g, fadeScreen.color.b, Mathf.MoveTowards(fadeScreen.color.a, 1f, fadeSpeed * Time.deltaTime));
+            if (fadeScreen.color.a == 1f)
+            {
+                fadeToBlack = false;
+            }
+        }
+    }
+
+    public void StartFadeToBlack()
+    {
+        fadeToBlack = true;
+        fadeOutBlack = false; 
     }
 }
