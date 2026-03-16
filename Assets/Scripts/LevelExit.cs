3bec9db6af69654fb7a16feb50e0f1c6d825c3ba
Dmitry Nazarov <roygbiv1969@yandex.ru>
2026-03-16 20:55:36 +0300

    Level Flow Updated/Exit/Fade

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class LevelExit : MonoBehaviour
+{
+    public string levelToLoad; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            //SceneManager.LoadScene(levelToLoad);
+
+            StartCoroutine(LevelManager.instance.LevelEnd());    
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -32,7 +32,11 @@ public class PlayerController : MonoBehaviour
     public float dashSpeed = 8f, dashLength = .5f, dashCooldown = 1f, dashInvinsibility = .5f;
     [HideInInspector] 
     public float dashCounter; 
-    private float dashCoolCounter; 
+    private float dashCoolCounter;
+
+    [HideInInspector]
+    public bool canMove = true; 
+
 
     private void Awake()
     {
@@ -48,94 +52,104 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-
-        moveInput.Normalize(); //fixing diagonal move 
-
-
-        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
-
-        theRB.linearVelocity = moveInput * activeMoveSpeed;
-
-
-        Vector3 mousePos =  Input.mousePosition;
-        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
-
-        if(mousePos.x < screenPoint.x)
+        if (canMove)
         {
-            transform.localScale = new Vector3(-1f,1f,1f);
-            gunArm.localScale = new Vector3(-1f, -1f, 1f); 
-        }else
-        {
-            transform.localScale = Vector3.one;
-            gunArm.localScale = Vector3.one; 
-        }
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
 
-        //rotate gun arm
-        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        gunArm.rotation = Quaternion.Euler(0,0,angle);
+            moveInput.Normalize(); //fixing diagonal move 
 
-        if(Input.GetMouseButtonDown(0)) //fire setup
-        {
-            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
-            shotCounter = timeBetweenShots;
-            AudioManager.instance.PlaySFX(12);
-        }
 
-        if(Input.GetMouseButton(0))
-        {
-            shotCounter -= Time.deltaTime;
+            //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
 
-            if (shotCounter <= 0)
+            theRB.linearVelocity = moveInput * activeMoveSpeed;
+
+
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+            if (mousePos.x < screenPoint.x)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                gunArm.localScale = new Vector3(-1f, -1f, 1f);
+            }
+            else
+            {
+                transform.localScale = Vector3.one;
+                gunArm.localScale = Vector3.one;
+            }
+
+            //rotate gun arm
+            Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+            float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+            gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+            if (Input.GetMouseButtonDown(0)) //fire setup
             {
                 Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
-                AudioManager.instance.PlaySFX(12);
-
                 shotCounter = timeBetweenShots;
+                AudioManager.instance.PlaySFX(12);
             }
-        }
 
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if (dashCoolCounter <= 0 && dashCounter <= 0)
+            if (Input.GetMouseButton(0))
             {
-                activeMoveSpeed = dashSpeed;
-                dashCounter = dashLength;
+                shotCounter -= Time.deltaTime;
 
-                anim.SetTrigger("dash");
-                PlayerHealthController.instance.MakeInvincible(dashInvinsibility);
+                if (shotCounter <= 0)
+                {
+                    Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                    AudioManager.instance.PlaySFX(12);
 
-                AudioManager.instance.PlaySFX(8);
-
+                    shotCounter = timeBetweenShots;
+                }
             }
-        }
 
-        if(dashCounter > 0)
-        {
-            dashCounter -= Time.deltaTime;
-            if(dashCounter <= 0)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                activeMoveSpeed = moveSpeed;
-                dashCoolCounter = dashCooldown; 
+                if (dashCoolCounter <= 0 && dashCounter <= 0)
+                {
+                    activeMoveSpeed = dashSpeed;
+                    dashCounter = dashLength;
+
+                    anim.SetTrigger("dash");
+                    PlayerHealthController.instance.MakeInvincible(dashInvinsibility);
+
+                    AudioManager.instance.PlaySFX(8);
+
+                }
             }
-        }
 
-        if (dashCoolCounter > 0)
-        {
-            dashCoolCounter -= Time.deltaTime;  
-        }
+            if (dashCounter > 0)
+            {
+                dashCounter -= Time.deltaTime;
+                if (dashCounter <= 0)
+                {
+                    activeMoveSpeed = moveSpeed;
+                    dashCoolCounter = dashCooldown;
+                }
+            }
+
+            if (dashCoolCounter > 0)
+            {
+                dashCoolCounter -= Time.deltaTime;
+            }
 
 
 
-        if (moveInput != Vector2.zero) //animation setup 
-        {
-            anim.SetBool("isMoving", true); 
+            if (moveInput != Vector2.zero) //animation setup 
+            {
+                anim.SetBool("isMoving", true);
+            }
+            else
+            {
+                anim.SetBool("isMoving", false);
+            }
         } else
         {
+            theRB.linearVelocity = Vector2.zero;
             anim.SetBool("isMoving", false);
-        }    
+        }
+
     }
 
 
