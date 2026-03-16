532c5f03436a2055deda289095579e535dc6d8b0
Dmitry_Nazarov <roygbiv1969@yandex.ru>
2026-03-16 23:53:16 +0300

    TitleScreen

---
+++ Assets/Scripts/Main Menu.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(levelToLoad);    
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(); 
+    }
+}
