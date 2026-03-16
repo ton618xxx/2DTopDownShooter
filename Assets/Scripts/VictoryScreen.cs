cd045278c14eaa12d4e864d845540ac8efae8381
Dmitry Nazarov <roygbiv1969@yandex.ru>
2026-03-17 00:18:32 +0300

    VictoryScreen

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class VictoryScreen : MonoBehaviour
+{
+    public float waitForAnyKey = 2f;
+
+    public GameObject anyKeyText;
+
+    public string mainMenuScene; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(waitForAnyKey > 0)
+        {
+            waitForAnyKey -= Time.deltaTime;
+            if(waitForAnyKey <= 0)
+            {
+                anyKeyText.SetActive(true);
+            }
+        }else
+        {
+            if(Input.anyKeyDown)
+            {
+                SceneManager.LoadScene(mainMenuScene); 
+            }
+        }
+    }
+}
