cf8a481997e7c8a4982a8cecc208edb5ba64d33e
Dmitry Nazarov <roygbiv1969@yandex.ru>
2026-04-15 23:36:25 +0300

    Center Room Pr + Fix Doors

---
+++ Assets/Scripts/RoomCenter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCenter : MonoBehaviour
+{
+
+    public bool openWhenEnemiesCleared; 
+
+    public List<GameObject> enemies = new List<GameObject>();
+
+    public Room theRoom; 
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (enemies.Count > 0 && openWhenEnemiesCleared && !theRoom.roomCleared)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+
+                if (enemies[i] == null || !enemies[i].activeInHierarchy)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+        }
+
+
+        if (openWhenEnemiesCleared && !theRoom.roomCleared && enemies.Count == 0)
+        {
+            theRoom.OpenDoors();
+        }
+    }
+}
